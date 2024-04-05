647eb38761e21dea1fd38606e0b40f59d4f12a79
suenalaba <joshuajr13@gmail.com>
2024-04-06 01:31:38 +0800

    feat: ✨add finetuned finbert model

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, render_template
 import google.generativeai as palm
-# from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
 
 load_dotenv()
 
@@ -13,23 +14,41 @@ gpt_model = {
 }
 palm.configure(api_key=MAKERSUITE_API_KEY)
 
-
-# tokenizer = AutoTokenizer.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
-# finbert_model = AutoModelForSequenceClassification.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
-
 app = Flask(__name__)
+
+def get_custom_prediction(model, tokenizer, text):
+    # Tokenize the input text
+    inputs = tokenizer(text, return_tensors="pt", padding=True)
+
+    # Move the inputs to the same device as the model
+    inputs = {k: v.to(model.device) for k, v in inputs.items()}
+
+    # Make the prediction
+    with torch.no_grad():
+        outputs = model(**inputs)
+        prediction = outputs.logits.argmax(dim=-1).item()
+
+    predicted_label = model.config.id2label[prediction]
+
+    return predicted_label
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST" and request.form.get("tweet"):
         tweet = request.form.get("tweet")
-        # inputs = tokenizer(tweet, return_tensors="pt")
-        # outputs = finbert_model(**inputs)
-        # logits = outputs.logits
-        # predicted_class_id = logits.argmax(-1).item()
-        # predicted_class_label = finbert_model.config.id2label[predicted_class_id] 
-        # return (render_template("index.html", result=predicted_class_label.upper()))
-        return (render_template("index.html", result=tweet))
+        model_dir = "model_config"
+        # loaded_model = AutoModelForSequenceClassification.from_pretrained(model_dir)
+        # loaded_tokenizer = AutoTokenizer.from_pretrained(model_dir)
+        # sentiment_output = get_custom_prediction(loaded_model, loaded_tokenizer, tweet)
+        tokenizer = AutoTokenizer.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+        finbert_model = AutoModelForSequenceClassification.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
+        inputs = tokenizer(tweet, return_tensors="pt")
+        outputs = finbert_model(**inputs)
+        logits = outputs.logits
+        predicted_class_id = logits.argmax(-1).item()
+        predicted_class_label = finbert_model.config.id2label[predicted_class_id] 
+        return (render_template("index.html", result=predicted_class_label.upper()))
+        # return (render_template("index.html", result=sentiment_output.upper()))
     else:
         return (render_template("index.html", result="Please key in a financial text or tweet."))
 
