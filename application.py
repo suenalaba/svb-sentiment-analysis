cbd1aa6ade97df3c73c7684ce5f4284549df576e
suenalaba <joshuajr13@gmail.com>
2024-04-11 21:13:23 +0800

    feat: ✨add tweet to interface

--- application.py
+++ application.py
@@ -24,10 +24,13 @@ def index():
         tweet = request.form.get("tweet")
         data = {"inputs": tweet}
         headers = {"Content-Type": "application/json"}
-        response = requests.post(SAGEMAKER_INFERENCE_ENDPOINT_URL, headers=headers, json=data)
-        api_response_data = response.json()
-        predicted_label = api_response_data["predicted_label"]
-        return (render_template("index.html", result=predicted_label+" sentiment."))
+        try:
+            response = requests.post(SAGEMAKER_INFERENCE_ENDPOINT_URL, headers=headers, json=data)
+            api_response_data = response.json()
+            predicted_label = api_response_data["predicted_label"]
+            return (render_template("index.html", result={"sentiment": predicted_label+" sentiment.", "tweet": tweet}))
+        except:
+            return (render_template("index.html", result="Service currently unavailable, please try again later."))
     else:
         return (render_template("index.html", result="Please key in a financial text or tweet."))
 
@@ -37,7 +40,7 @@ def financegpt():
         tweet = request.form.get("tweet")
         prompt = "Is this financial text a positive or negative sentiment: " + tweet
         result = palm.chat(**gpt_model, messages=prompt)
-        return (render_template("financegpt.html", result=result.last))
+        return (render_template("financegpt.html", result={"sentiment": result.last, "tweet": tweet}))
 
     else:
         return (render_template("financegpt.html", result="Please key in a financial text or tweet."))
@@ -45,16 +48,19 @@ def financegpt():
 @application.route("/stockpriceanalyzer", methods=["GET", "POST"])
 def stockpriceanalyzer():
     if request.method == "POST" and request.form.get("selected_symbol"):
-        selected_symbol = request.form.get("selected_symbol").upper()
-        alphavantage_api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={selected_symbol}&outputsize=compact&apikey={ALPHAVANTAGE_API_KEY}'
-        r = requests.get(alphavantage_api_url)
-        data = r.json()
-        symbol = data['Meta Data']['2. Symbol']
-        last_30_days = {k: data['Time Series (Daily)'][k] for k in list(data['Time Series (Daily)'])[:30]}
-        main_prompt = f'Given the last 30 traded days for {symbol} stock: [{last_30_days}], DO NOTE THAT the prices are most recent date first analyze the following:'
-        prompt = main_prompt + analysis.analysis
-        result = palm.chat(**gpt_model, messages=prompt)
-        return (render_template("stockpriceanalyzer.html", result=result.last))
+        try:
+            selected_symbol = request.form.get("selected_symbol").upper()
+            alphavantage_api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={selected_symbol}&outputsize=compact&apikey={ALPHAVANTAGE_API_KEY}'
+            r = requests.get(alphavantage_api_url)
+            data = r.json()
+            symbol = data['Meta Data']['2. Symbol']
+            last_30_days = {k: data['Time Series (Daily)'][k] for k in list(data['Time Series (Daily)'])[:30]}
+            main_prompt = f'Given the last 30 traded days for {symbol} stock: [{last_30_days}], DO NOTE THAT the prices are most recent date first analyze the following:'
+            prompt = main_prompt + analysis.analysis
+            result = palm.chat(**gpt_model, messages=prompt)
+            return (render_template("stockpriceanalyzer.html", result={"analysis": result.last, "symbol": symbol}))
+        except:
+            return (render_template("stockpriceanalyzer.html", result="Please enter a valid stock symbol."))
     else:
         return (render_template("stockpriceanalyzer.html", result="Please enter a stock symbol."))
 
