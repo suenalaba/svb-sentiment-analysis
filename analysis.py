2a4bcbbfaabfdfc14ba16d498744ccb6d99729ec
suenalaba <joshuajr13@gmail.com>
2024-04-11 19:40:38 +0800

    feat: ✨add stockprizeanalyzer

---
+++ analysis.py
@@ -0,0 +1,22 @@
+analysis = """
+1. Overall price trend:
+   - Calculate the percentage change between the first and last data points.
+   - Identify any significant price swings or patterns during the period.
+
+2. Moving Averages:
+   - Calculate the 20-day and 50-day simple moving averages (SMAs).
+   - Analyze the relationship between the price and the moving averages (e.g., golden cross, death cross).
+
+3. MACD (Moving Average Convergence Divergence):
+   - Calculate the MACD using a 12-day EMA for the faster line and a 26-day EMA for the slower line.
+   - Analyze the MACD line and the MACD histogram for potential buy or sell signals.
+
+4. Relative Strength Index (RSI):
+   - Calculate the 14-day RSI.
+   - Identify periods when the RSI is overbought (above 70) or oversold (below 30).
+
+5. Conclusion:
+   - Based on the analysis of the above indicators, provide a detailed assessment of the stocks potential future trend (bullish, bearish, or range-bound).
+   - Highlight any divergences or convergences between the indicators.
+   - Suggest potential entry or exit points based on the analysis.
+"""
--- app.py
+++ app.py
@@ -2,57 +2,36 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request, render_template
 import google.generativeai as palm
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
+import requests
+import analysis
 
 load_dotenv()
 
+ALPHAVANTAGE_API_KEY = os.getenv('ALPHAVANTAGE_API_KEY')
 MAKERSUITE_API_KEY = os.getenv('MAKERSUITE_API_KEY')
+SAGEMAKER_INFERENCE_ENDPOINT_URL = os.getenv('SAGEMAKER_INFERENCE_ENDPOINT_URL')
 
 gpt_model = {
     "model": "models/chat-bison-001"
 }
 palm.configure(api_key=MAKERSUITE_API_KEY)
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-def get_custom_prediction(model, tokenizer, text):
-    # Tokenize the input text
-    inputs = tokenizer(text, return_tensors="pt", padding=True)
-
-    # Move the inputs to the same device as the model
-    inputs = {k: v.to(model.device) for k, v in inputs.items()}
-
-    # Make the prediction
-    with torch.no_grad():
-        outputs = model(**inputs)
-        prediction = outputs.logits.argmax(dim=-1).item()
-
-    predicted_label = model.config.id2label[prediction]
-
-    return predicted_label
-
-@app.route("/", methods=["GET", "POST"])
+@application.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST" and request.form.get("tweet"):
         tweet = request.form.get("tweet")
-        model_dir = "model_config"
-        loaded_model = AutoModelForSequenceClassification.from_pretrained(model_dir)
-        loaded_tokenizer = AutoTokenizer.from_pretrained(model_dir)
-        sentiment_output = get_custom_prediction(loaded_model, loaded_tokenizer, tweet)
-        # tokenizer = AutoTokenizer.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
-        # finbert_model = AutoModelForSequenceClassification.from_pretrained("mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
-        # inputs = tokenizer(tweet, return_tensors="pt")
-        # outputs = finbert_model(**inputs)
-        # logits = outputs.logits
-        # predicted_class_id = logits.argmax(-1).item()
-        # predicted_class_label = finbert_model.config.id2label[predicted_class_id] 
-        # return (render_template("index.html", result=predicted_class_label.upper()))
-        return (render_template("index.html", result=sentiment_output.upper()))
+        data = {"inputs": tweet}
+        headers = {"Content-Type": "application/json"}
+        response = requests.post(SAGEMAKER_INFERENCE_ENDPOINT_URL, headers=headers, json=data)
+        api_response_data = response.json()
+        predicted_label = api_response_data["predicted_label"]
+        return (render_template("index.html", result=predicted_label+" sentiment."))
     else:
         return (render_template("index.html", result="Please key in a financial text or tweet."))
 
-@app.route("/financegpt", methods=["GET", "POST"])
+@application.route("/financegpt", methods=["GET", "POST"])
 def financegpt():
     if request.method == "POST" and request.form.get("tweet"):
         tweet = request.form.get("tweet")
@@ -63,11 +42,27 @@ def financegpt():
     else:
         return (render_template("financegpt.html", result="Please key in a financial text or tweet."))
 
-@app.route("/end", methods=["GET", "POST"])
+@application.route("/stockpriceanalyzer", methods=["GET", "POST"])
+def stockpriceanalyzer():
+    if request.method == "POST" and request.form.get("selected_symbol"):
+        selected_symbol = request.form.get("selected_symbol").upper()
+        alphavantage_api_url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={selected_symbol}&outputsize=compact&apikey={ALPHAVANTAGE_API_KEY}'
+        r = requests.get(alphavantage_api_url)
+        data = r.json()
+        symbol = data['Meta Data']['2. Symbol']
+        last_30_days = {k: data['Time Series (Daily)'][k] for k in list(data['Time Series (Daily)'])[:30]}
+        main_prompt = f'Given the last 30 traded days for {symbol} stock: [{last_30_days}], DO NOTE THAT the prices are most recent date first analyze the following:'
+        prompt = main_prompt + analysis.analysis
+        result = palm.chat(**gpt_model, messages=prompt)
+        return (render_template("stockpriceanalyzer.html", result=result.last))
+    else:
+        return (render_template("stockpriceanalyzer.html", result="Please enter a stock symbol."))
+
+@application.route("/end", methods=["GET", "POST"])
 def end():
     if request.method == "POST":
         print(request.form)
         return render_template("end.html")
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    application.run()
